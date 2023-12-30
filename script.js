ffedea01c6af670450b76e286257fe154236fd97
ElvisWesley <trymwo2222@gmail.com>
2023-12-30 16:55:44 +0100

    Added some stored messages

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Test");
+//https://fallout.fandom.com/wiki/Fallout_76_legendary_weapon_effects
+const firstRoll = ["Bloodied", "Junkie", "Aristocrat", "Mutant"];
+const secondRoll = [" +25% Faster firerate", " +50% crit damage", "-25% vats cost", "explosive bullets"];
+const thirdRoll = [" +1 Agility", " +1 Intelligence", "+1 Strength", "+1 Charisma"];
